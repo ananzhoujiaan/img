017d4b49a19f46788de2717f17b41801d3f8e1c7
zhou jiaan <1540774503@qq.com>
2018-07-06 11:57:25 +0800

    add $fileline

--- true.sh
+++ true.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fileline=`tail -n 1 /var/log/img.log|awk -F '/' '{print $3}'`
-git add /img/$fileline
-git commit -a -m 'add $fileline'
-git push origin master
+git add /img/$fileline >>/dev/null
+git commit -a -m 'add $fileline' >>/dev/null
+git push origin master >>/dev/null
 echo https://raw.githubusercontent.com/ananzhoujiaan/img/master/$fileline
